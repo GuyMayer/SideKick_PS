ee157f1a9959321bdbfd78b0b9a1bce53ec0e35a
GuyMayer <guy@zoomphotography.co.uk>
2026-02-01 15:49:21 +0000

    v2.4.49 release

--- run_build.bat
+++ run_build.bat
@@ -1,5 +1,5 @@
 @echo off
 cd /d "C:\Stash\SideKick_PS"
 call C:\Stash\.venv\Scripts\activate.bat
-powershell -ExecutionPolicy Bypass -File "build_and_archive.ps1" -Version "2.4.48"
+powershell -ExecutionPolicy Bypass -File "build_and_archive.ps1" -Version "2.4.49"
 pause
